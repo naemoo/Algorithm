9ceb9872e5cbc6941f85d0c6ed87980351b24c09
gusdn <gusdn@DESKTOP-D51OCDK>
2019-08-21 14:50:55 +0900

    프로그래머스 : 여행경로(DFS/BFS)

--- src/Search/Problem17.java
+++ src/Search/Problem17.java
@@ -49,6 +49,7 @@ public class Problem17 {
 		Solution17 s = new Solution17();
 		String[][] tickets = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
 //		String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
+		System.out.print("answer : ");
 		for(String str : s.solution(tickets))
 			System.out.print(str+" ");
 	}
