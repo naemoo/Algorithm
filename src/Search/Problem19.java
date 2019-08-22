c3fda1cd8748aa16e7874977c7cfde94608e3035
gusdn <gusdn@DESKTOP-D51OCDK>
2019-08-22 14:36:12 +0900

    프로그래머스 : 카펫(완전탐색)

--- src/Search/Problem19.java
+++ src/Search/Problem19.java
@@ -4,6 +4,7 @@ package Search;
  * 프로그래머스 : 카펫(완전탐색)
  * https://programmers.co.kr/learn/courses/30/lessons/42842
 */
+//에라토스테네스의 체 사용
 class Solution19{
 	public int[] solution(int brown,int red) {
 		int sum = brown + red;
