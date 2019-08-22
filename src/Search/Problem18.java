2e821372d4ed24d6451ec9bc1508535f03a338a8
gusdn <gusdn@DESKTOP-D51OCDK>
2019-08-22 14:33:00 +0900

    프로그래머스 : 숫자야구(완전탐색)

--- src/Search/Problem18.java
+++ src/Search/Problem18.java
@@ -54,7 +54,7 @@ class Solution {
     }
 }
 
-public class problem18{
+public class Problem18{
 	public static void main(String[] args) {
 		Solution s = new Solution();
 		int answer = s.solution(new int[][] {{123,1,1},{356,1,0},{327,2,0},{489,0,1}});
