32b196894607f6fbac3a7267840a571959eebae7
naemoo <gusdn0651@naver.com>
2020-07-02 15:10:48 +0900

    카카오 블라인드 코딩 테스트(오픈 채팅방)

---
+++ src/Kakao/Problem1.java
@@ -0,0 +1,40 @@
+package Kakao;
+
+import java.util.*;
+
+/*
+ * 2019 KAKAO BLIND RECRUITMENT
+ * 오픝 채팅방 문제
+ * https://programmers.co.kr/learn/courses/30/lessons/42888
+ */
+
+public class Problem1 {
+	static public String[] solution(String[] record) {
+		HashMap<String, String> user_map = new HashMap<>();
+		List<String> ans = new LinkedList<>();
+		for(String cmd:record) {
+			String[] tok = cmd.split(" ");
+			if(tok[0].equals("Enter")||tok[0].equals("Change")) {
+				user_map.put(tok[1], tok[2]);
+			}
+		}
+		for(String cmd:record) {
+			String[] tok = cmd.split(" ");
+			if(tok[0].equals("Enter")) {
+				ans.add(user_map.get(tok[1])+"님이 들어왔습니다.");
+			}
+			else if(tok[0].equals("Leave")) {
+				ans.add(user_map.get(tok[1])+"님이 나갔습니다.");
+			}
+		}
+		return ans.toArray(String[]::new);
+    }
+	
+	public static void main(String[] args) {
+		String[] record = new String[] {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+		String[] strs = solution(record);
+		for(String str:strs) {
+			System.out.println(str);
+		}
+	}
+}
