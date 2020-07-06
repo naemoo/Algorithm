8a38fe61289375d62ea385d6a978e098c13103c3
naemoo <gusdn0657@gmail.com>
2020-07-06 23:46:08 +0900

    부분집합의 합

---
+++ src/DataStructure/Problem2.java
@@ -0,0 +1,34 @@
+/*
+ * https://www.acmicpc.net/problem/1182
+ * 부분 집합의 합(집합 문제)
+ */
+
+package DataStructure;
+
+import java.util.*;
+import java.io.*;
+
+public class Problem2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num = Integer.parseInt(st.nextToken());
+		int sum = Integer.parseInt(st.nextToken());
+		int[] arr = new int[num];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0 ;i<num;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=0;i< 1<<num;i++) {
+			int subsetSum = 0;
+			for(int subset = 0;subset < num;i++) {
+				if((subset&i) == subset) {
+					subsetSum += subset;
+				}
+			}
+		}
+	}
+}
