3aa125d6a43c9060d32b987c8b920d8cc0e701a0
naemoo <gusdn0657@gmail.com>
2020-07-11 15:32:06 +0900

    카카오 블라인드 코딩 테스트(압축 문제)

---
+++ src/Kakao/Problem7.java
@@ -0,0 +1,51 @@
+/*
+ * 2018 KAKAO BLIND RECRUITMENT
+ * https://programmers.co.kr/learn/courses/30/lessons/17684
+ * 압축 문제
+*/
+package Kakao;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String msg) {
+    	Queue<String> queue = new LinkedList<>();
+    	List<Integer> answer = new LinkedList<>();
+    	HashMap<String, Integer> dictionary = new HashMap<>();
+    	int dictIdx = 1;
+    	
+    	for(char c = 'A';c<='Z';c++) {
+    		dictionary.put(String.valueOf(c), dictIdx++);
+    	}
+    	
+    	for(String str:msg.split("")) {
+    		queue.add(str);
+    	}
+    	
+    	String w;
+    	String c;
+    	while(!queue.isEmpty()) {
+    		w = queue.poll();
+    		c = queue.peek();
+    		while(c != null
+    				&&(dictionary.containsKey(w+c))) {
+    			w = w + queue.poll();
+    			c = queue.peek();
+    		}
+    		answer.add(dictionary.get(w));
+    		dictionary.put(w+c,dictIdx++);
+    	}
+    	return answer.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
+
+
+public class Problem7 {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int[] arr = s.solution("ABABABABABABABAB");
+		for(int i:arr) {
+			System.out.println(i);
+		}
+	}
+}
