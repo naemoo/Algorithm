755f442d6850d998a80b3c2d584dad3ab5fa3bea
naemoo <gusdn0657@gmail.com>
2020-07-13 17:38:59 +0900

    카카오 블라인드 코딩 테스트(파일명 정렬 문제)

--- src/Kakao/Problem08.java
+++ src/Kakao/Problem08.java
@@ -7,8 +7,8 @@ package Kakao;
 
 import java.util.*;
 
-class Solution {
-    class File implements Comparable<File>{
+public class Problem08 {
+	static class File implements Comparable<File>{
     	String orgName;
         String head ="";
         String number ="";
@@ -50,7 +50,7 @@ class Solution {
 		}
     }
 
-    public String[] solution(String[] files) {
+    static public String[] solution(String[] files) {
     	List<File> list = new LinkedList<>();
     	for(String file:files) {
     		list.add(new File(file));
@@ -58,16 +58,11 @@ class Solution {
     	Collections.sort(list);
     	return list.stream().map(str->str.orgName).toArray(String[]::new);
     }
-}
-
-public class Problem8 {
+	
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        String[] answers =
-                solution.solution(new String[]{"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"});
+        String[] answers = solution(new String[]{"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"});
         for(String answer:answers){
             System.out.println(answer);
         }
-    	
     }
 }
