5b555b713d27814a5742811a35200c9afa35bcd1
naemoo <gusdn0657@gmail.com>
2020-07-19 22:23:11 +0900

    2019 카카오 인턴십(튜플)

--- src/Kakao/Problem15.java
+++ src/Kakao/Problem15.java
@@ -6,24 +6,20 @@
 package Kakao;
 
 import java.util.*;
-import java.util.stream.*;
+import java.util.regex.*;
 
 public class Problem15 {
-	 public static int[] solution(String s) {
-		 String[] subsets = s.substring(2,s.length()-2).split("\\},\\{");
-		 HashMap<String,Integer> elementDict = new HashMap<>();
-		 
-		 for(String subset:subsets) 
-			 for(String element: subset.split(",")) 
-				 elementDict.put(element,elementDict.getOrDefault(element, 0)+1);
-			 
-	     return elementDict.entrySet().stream()
-	    		 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-	    		 .map(set->set.getKey()).mapToInt(Integer::valueOf).toArray();
-	    }
+	public static int[] solution(String s) {
+		HashMap<String,Integer> map = new HashMap<>();
+		Pattern pattern = Pattern.compile("[0-9]");
+		Matcher matcher = pattern.matcher(s);
+		while(matcher.find()) 
+			map.put(matcher.group(),map.getOrDefault(matcher.group(), 0)+1);
+		return map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				.map(set->set.getKey()).mapToInt(Integer::parseInt).toArray();
+	}
 	 
 	public static void main(String[] args) {
 		int[] ans = solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
 		System.out.println(Arrays.toString(ans));
-	}
-}
+	}}
