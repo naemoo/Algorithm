e9764c5c2be2f5b56bd4bf2eccf40706e08ee163
naemoo <gusdn0657@gmail.com>
2020-08-22 21:51:59 +0900

    디스크컨트롤러(Heap)

--- src/DataStructure/Problem4.java
+++ src/DataStructure/Problem4.java
@@ -5,11 +5,11 @@
 package DataStructure;
 
 import java.util.*;
+import java.util.stream.*;
 
 public class Problem4 {
 
     static class Process implements Comparable<Process> {
-	static int end = 0;
 	int request;
 	int consume;
 
@@ -21,32 +21,60 @@ public class Problem4 {
 
 	@Override
 	public int compareTo(Process p) {
-	    if (request <= end && p.request <= end) {
-	    } else {
-		return request <= end ? -1 : 0;
-	    }
-	    return 0;
+	    return Integer.compare(consume, p.consume);
+	}
+
+	public int getWorkingTime(int end) {
+	    return end - request + consume;
 	}
 
 	@Override
 	public String toString() {
 	    return "Process [request=" + request + ", consume=" + consume + "]";
 	}
-
     }
 
     public static int solution(int[][] jobs) {
-	PriorityQueue<Process> q = new PriorityQueue<>();
+	PriorityQueue<Process> waitQ = new PriorityQueue<>();
+	Queue<Process> processes = Arrays.stream(jobs)
+		.sorted((a,b)->Arrays.compare(a, b))
+		.flatMap(job -> Stream.of(new Process(job[0], job[1])))
+		.collect(Collectors.toCollection(LinkedList::new));
+	int answer = 0;
+	int end = 0;
 
-	for (int[] job : jobs) {
-	    
+	while (!processes.isEmpty() || !waitQ.isEmpty()) {
+
+	    while (!processes.isEmpty()) {
+		Process tmp = processes.peek();
+		if (tmp.request <= end) {
+		    waitQ.add(processes.poll());
+		} else {
+		    break;
+		}
+	    }
+
+	    if (waitQ.isEmpty()) {
+		Process tmp = processes.poll();
+		answer += tmp.consume;
+		end = tmp.request;
+		end += tmp.consume;
+	    } else {
+		Process tmp = waitQ.poll();
+		answer += tmp.getWorkingTime(end);
+		end += tmp.consume;
+	    }
 	}
-
-	return 0;
+	System.out.println(answer / jobs.length);
+	return answer / jobs.length;
     }
 
     public static void main(String[] args) {
-	solution(new int[][] { { 0, 3 }, { 1, 9 }, { 2, 1 } });
+	solution(new int[][] { { 0, 3 }, { 1, 9 }, { 2, 6 } });
+	solution(new int[][] { { 0, 9 }, { 0, 4 }, { 0, 5 }, { 0, 7 }, { 0, 3 } });
+	solution(new int[][] { { 1, 9 }, { 1, 4 }, { 1, 5 }, { 1, 7 }, { 1, 3 } });
+	solution(new int[][] { { 0, 3 }, { 1, 9 }, { 500, 6 } });
+
     }
 
 }
