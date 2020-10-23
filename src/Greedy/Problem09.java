4c75c4fec8c563709a877021c868268c45954147
naemoo <gusdn0657@gmail.com>
2020-10-23 23:51:30 +0900

    여행경로(다익스트라 - Heap)

--- src/Greedy/Problem09.java
+++ src/Greedy/Problem09.java
@@ -1,12 +1,10 @@
 package Greedy;
 
 import java.util.*;
-import java.util.Map.Entry;
 import java.io.*;
 
 public class Problem09 {
     static HashMap<Integer, HashMap<Integer, Integer>> infos;
-    static boolean[] visit;
     static int min = Integer.MAX_VALUE;
     static int n;
 
@@ -15,7 +13,6 @@ public class Problem09 {
 	String[] nm = br.readLine().split("\\s");
 	n = Integer.parseInt(nm[0]);
 	int m = Integer.parseInt(nm[1]);
-	visit = new boolean[n + 1];
 	infos = new HashMap<>();
 
 	for (int i = 0; i < m; i++) {
@@ -23,7 +20,7 @@ public class Problem09 {
 	    int start = Integer.parseInt(info[0]);
 	    int end = Integer.parseInt(info[1]);
 	    int weight = Integer.parseInt(info[2]);
-	    
+
 	    HashMap<Integer, Integer> airplane = infos.computeIfAbsent(start, k -> new HashMap<>());
 
 	    if (airplane.containsKey(end)) {
@@ -32,24 +29,36 @@ public class Problem09 {
 		airplane.put(end, weight);
 	    }
 	}
-	dfs(0, 1);
 
-	System.out.println(min != Integer.MAX_VALUE ? min : "go home");
+	dijkstra(1);
+
     }
 
-    private static void dfs(int d, int start) {
-	if (start == n) {
-	    min = Integer.min(min, d);
-	}
+    private static void dijkstra(int start) {
+	int[] length = new int[n + 1];
+	Arrays.fill(length, Integer.MAX_VALUE);
+	Queue<int[]> q = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[1], b[1]));
+	q.add(new int[] { start, 0 });
 
-	if (infos.containsKey(start)) {
-	    for (Entry<Integer, Integer> info : infos.get(start).entrySet()) {
-		if (!visit[info.getKey()]) {
-		    visit[info.getKey()] = true;
-		    dfs(d + info.getValue(), info.getKey());
-		    visit[info.getKey()] = false;
+	while (!q.isEmpty()) {
+	    int cur = q.peek()[0];
+	    int dis = q.poll()[1];
+	    // 갱신이 된 상태라면 무시하고 넘어감
+	    if (length[cur] < dis)
+		continue;
+
+	    if (infos.containsKey(cur)) {
+		for (Map.Entry<Integer, Integer> entry : infos.get(cur).entrySet()) {
+		    int next = entry.getKey();
+		    int nxtDis = dis + entry.getValue();
+
+		    if (nxtDis < length[next]) {
+			length[next] = nxtDis;
+			q.add(new int[] { next, nxtDis });
+		    }
 		}
 	    }
 	}
+	System.out.println(length[n] != Integer.MAX_VALUE ? length[n] : "go home");
     }
 }
