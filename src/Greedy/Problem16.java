1f7c2fa5ca8715492006c2b18648b9471b990acf
naemoo <gusdn0657@gmail.com>
2020-11-13 18:10:34 +0900

    백준 : 비용

---
+++ src/Greedy/Problem16.java
@@ -0,0 +1,14 @@
+package Greedy;
+
+import java.util.*;
+import java.io.*;
+
+public class Problem16 {
+
+    public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+
+    }
+
+}
